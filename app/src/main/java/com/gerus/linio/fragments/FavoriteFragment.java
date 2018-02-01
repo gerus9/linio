4905d8290a6f45e9f835ba3d843d4060bc945ee9
gerus9 <gerus_dy@hotmail.com>
2018-02-01 00:55:39 -0600

    Implemented module favorites.

--- app/src/main/java/com/gerus/linio/fragments/FavoriteFragment.java
+++ app/src/main/java/com/gerus/linio/fragments/FavoriteFragment.java
@@ -4,27 +4,50 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.gerus.linio.OnGeneralInterface;
 import com.gerus.linio.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by gerus-mac on 22/01/17.
  */
 
 public class FavoriteFragment extends Fragment {
+    @BindView(R.id.rvFavorites)
+    RecyclerView rvFavorties;
 
     protected final String TAG = this.getClass().getSimpleName();
     private OnGeneralInterface mListener;
     protected Context mContext;
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        mContext = getActivity();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.favorities, menu);
     }
 
     @Override
